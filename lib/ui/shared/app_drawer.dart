7bf844ee686d38165caeb9c3ec783a0ce3febb25
Neko19o7 <vanb1910481@student.ctu.edu.vn>
2022-10-28 14:25:25 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/ui/shared/app_drawer.dart
+++ lib/ui/shared/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../auth/auth_manager.dart';
 import '../orders/orders_screen.dart';
 import'../products/user_products_screen.dart';
 
@@ -41,6 +43,17 @@ class AppDrawer extends StatelessWidget {
                 .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context)
+                ..pop()
+                ..pushReplacementNamed('/');
+                context.read<AuthManager>().logout();
+            },
+          )
         ],
       ),
     );
